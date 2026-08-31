b46fc1bb5923b6f567902ecc155576748a3d5ac1
ashuchudasama505-ops <ashuchudasama505@gmail.com>
2026-08-31 08:41:49 +0530

    reverse digit or loop
    How the Loop WorksInitialize a variable named reversed to 0.Run a loop while the original number is greater than 0 (or not equal to 0 for negative numbers).Extract the last digit using digit = num % 10.Update the reversed number by multiplying the current total by 10 and adding the new digit (reversed = reversed * 10 + digit).Remove the last digit from the original number using integer division (num = num / 10

---
+++ reverse digit.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int num,reverse=0,digit;
+	
+	printf("enter a number:");
+	 scanf("%d",&num);
+	 
+	 while(num!=0)
+	 
+	 	{
+	 			digit=num %10;
+	 			
+	 				reverse=reverse*10+digit;
+	 				
+	 				num=num/10;
+	 				
+		 }
+		 printf("reversed number=%d",reverse);
+		 
+		 return 0;
+}
