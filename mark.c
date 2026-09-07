b72afe380c2528c6563e60b7fcb9162484735606
ashuchudasama505-ops <ashuchudasama505@gmail.com>
2026-09-07 08:38:08 +0530

    C programs for vote, age, and marks using if-else.
    This repository contains basic C language programs for beginners:
    
    🗳️ Vote eligibility program
    🎂 Age checking program
    📚 Marks pass/fail program

---
+++ mark.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int mark;
+	printf("enter your mark ");
+	scanf("%d",&mark);
+	if (mark>=35)
+	{
+		printf("result pass");
+	}
+	else 
+		{
+			printf("result file");
+			}	
+			return 0;
+}
